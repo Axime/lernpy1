6811661d4afa4f741cb77ab17af919f021a736a5
Axime <ivashka_2020@list.ru>
2022-09-03 19:00:13 +0500

    zenker help

--- maincommandslist.py
+++ maincommandslist.py
@@ -1,28 +1,35 @@
 import sqlite3
-
+command = ""
 def input_command ():
+    global command
     print('Введите команду\n')
-    command = input()
-    if command in fun:
-        fun[command]()
+    command = input().split()
+    if str(command[0]) in fun:
+        fun[command[0]]()
     else:
         print(f'Команда {command} не найдена\n')
+    return command
 
 def help_c():
     print('Это небольшая программа для регистрации и идентификации пользователей посредством ID ключей by Axime')
     print("Список команд:")
     for keys in fun.keys():
         print(keys)
-    print ('\n')
 
 def list_users():
-    pass
-    '''
-    with open('users.txt', 'r') as f:
-        nums = f.read().splitlines()
-    print(nums)'''
+    con = sqlite3.connect('users.db')
+    c = con.cursor()
+    c.execute("SELECT rowid, * FROM users")
+
+    items = c.fetchall()
+    for item  in items:
+        print(str(item[0]) + " " + item[1] + " " + item[2])
+    con.commit()
+    con.close()
 
 def add_user():
+    user = str(command[1])
+    inf = str(command[2])
     con = sqlite3.connect('users.db')
 
     c = con.cursor()
@@ -32,22 +39,26 @@ def add_user():
 #        inf text
 #    )""")
 
-#    c.execute("INSERT INTO users VALUES ('SEREGA', '3D')")
+    c.execute("INSERT INTO users VALUES ('?', '?')", (str(user), str(inf)))
 
-    c.execute("SELECT * FROM users ")
-    print(c.fetchmany(5))
 
+    print(f'Запись {user} {inf} добавлена')
+#После названия таблицы идут условия выборки
+#where
+    c.execute("SELECT rowid, * FROM users WHERE rowid >= 1")
+    print(c.fetchall())
 
     con.commit()
 
     con.close()
-
-
+def create_table(name_table):
+    pass
 
 fun = {
     'help': help_c,
     'list': list_users,
-    'add': add_user
+    'add': add_user,
+    'create table': create_table
 }
 
 
