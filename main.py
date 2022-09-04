95e6194253b767980e0489a3a0ebe68c0caa1480
Axime <ivashka_2020@list.ru>
2022-09-04 18:01:27 +0500

    tkinter test

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 import maincommandslist
-from tkinter import *
-
 # луп ввода команд
 
-"""
+
 programm_work = True
 while programm_work:
     maincommandslist.input_command()
-"""
-
-window = Tk()
-window.title("AxiDataBase editor")
-window.geometry('400x250')
-
-
-but = Button(window, text = 'rand int', command=lambda: maincommandslist.rand_int(None))
-but.grid(column=1, row=0)
-
-window.mainloop()
