2812c5f4e7223a12a80f6a2f233581854a8ec844
Axime <ivashka_2020@list.ru>
2022-09-04 20:28:58 +0500

    tkinter test

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import maincommandslist
+import tkcom
 # луп ввода команд
 
-
+"""
 programm_work = True
 while programm_work:
     maincommandslist.input_command()
+"""
+tkcom.init_window()
---
+++ tkcom.py
@@ -0,0 +1,11 @@
+#Imporl list
+from tkinter import *
+
+
+
+#Command with window
+
+def init_window():
+    window = Tk()
+    window.title("Data Base Editor by Axime")
+    window.mainloop()
