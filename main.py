bc9726c5ae46c740a6f368429b14765a6f4c7045
Axime <ivashka_2020@list.ru>
2022-09-04 13:44:14 +0500

    add tkinter

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import maincommandslist
+from tkinter import *
 
 # система ввода команд
+"""
 programm_work = True
 while programm_work:
     maincommandslist.input_command()
+"""
+window = Tk()
+window.title("AxiDataBase editor")
+window.geometry('400x250')
+
+
+but = Button(window, text = '123', command=lambda: maincommandslist.rand_int(None))
+but.grid(column=1, row=0)
+
+window.mainloop()
